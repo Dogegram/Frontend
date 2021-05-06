1589356cbf3a2f67bfee22962d3fbe108b0ff69f
hrichiksite <hrichik.site@gmail.com>
2021-05-06 16:03:23 +0530

    Replace localhost with env

--- src/services/socketService.js
+++ src/services/socketService.js
@@ -1,7 +1,7 @@
 import io from 'socket.io-client';
 
 export const connect = () => {
-  const socket = io('http://localhost:5000',{
+  const socket = io(process.env.REACT_APP_BACKEND_URL,{
     query: { token: localStorage.getItem('token') },
   });
   return socket;
